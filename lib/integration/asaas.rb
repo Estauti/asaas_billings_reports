740dc4d538e92b6ad85b2fb499887a4a32b1e055
Estauti <estautib@gmail.com>
2021-02-11 21:39:04 -0300

    Add container class to application

--- lib/integration/asaas.rb
+++ lib/integration/asaas.rb
@@ -9,7 +9,7 @@ module AsaasAPI
   end
 
   def AsaasAPI.get_client_payments client_id
-    AsaasAPI.get_request("payments?limit=100&customer=#{client_id}&dateCreated[ge]=2020-09-01&dateCreated[le]=2020-09-30")
+    AsaasAPI.get_request("payments?limit=100&customer=#{client_id}&dateCreated[ge]=2021-01-01&dateCreated[le]=2021-02-15")
   end
 
   def AsaasAPI.get_payment payment_id
